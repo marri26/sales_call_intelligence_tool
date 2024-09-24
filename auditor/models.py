0864b43e455d3538d187b5bd9dbc4479ebbe3605
Mani Gidijala <manikhanta0023@gmail.com>
2024-09-25 00:36:57 +0530

    Added Call and Analysis Models

--- auditor/models.py
+++ auditor/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Call(models.Model):
+    def __str__(self):
+        return self.call_id
+
+    call_url = models.CharField(max_length=200)
+    call_id = models.CharField(max_length=200)
+    audit_datetime = models.DateTimeField("date published")
+
+
+class Analysis(models.Model):
+    def __str__(self):
+        return self.prompt_response
+    call_id = models.ForeignKey(Call, on_delete=models.CASCADE)
+    prompt_response = models.CharField(max_length=200)
+   
