00e0a4e1af52242a7fe50dda78016f315e4bb6a2
Mani Gidijala <manikhanta0023@gmail.com>
2024-09-25 23:15:18 +0530

    Added basic UI and linted code

--- auditor/admin.py
+++ auditor/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Call, Analysis, PromptConfig, CallTypeConfig
 
+
 class CallAdmin(admin.ModelAdmin):
-    list_display = ('call_url', 'id','audit_datetime')
-    list_filter = ['audit_datetime']
-    search_fields = ['call_url']
+    list_display = ("call_url", "id", "audit_datetime")
+    list_filter = ["audit_datetime"]
+    search_fields = ["call_url"]
+
 
 admin.site.register(Call, CallAdmin)
 admin.site.register(Analysis)
--- auditor/views.py
+++ auditor/views.py
@@ -1,4 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 def index(request):
     return HttpResponse("Auditor Page")
+
+
+def call_auditor(request):
+    if request.method == 'POST':
+        url = request.POST.get('call_auditor')
+        
+        if not url:
+            return render(request, 'call_auditor.html', {'error_message': 'Please enter a valid URL'})
+        
+
+        
+        return HttpResponse(f'Call audit initiated for: {url}')
+    
+    return render(request, 'call_auditor.html')
