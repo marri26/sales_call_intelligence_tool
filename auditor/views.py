9b5a314e9df7d2a81c8e8c0a9c2e1039022d6294
Mani Gidijala <manikhanta0023@gmail.com>
2024-09-28 16:05:15 +0530

    Added succes, failure pages and logic to create records in db

--- auditor/views.py
+++ auditor/views.py
@@ -1,19 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from .forms import AuditForm
+from django.utils import timezone
 
 def index(request):
     return HttpResponse("Auditor Page")
 
-
 def call_auditor(request):
     if request.method == 'POST':
-        url = request.POST.get('call_auditor')
-        
-        if not url:
-            return render(request, 'call_auditor.html', {'error_message': 'Please enter a valid URL'})
-        
+        form = AuditForm(request.POST)
 
-        
-        return HttpResponse(f'Call audit initiated for: {url}')
-    
-    return render(request, 'call_auditor.html')
+        if form.is_valid():
+            call = form.save(commit=False)
+            call.audit_status = "SUCCESS"  # Set to SUCCESS if the form is valid
+            call.call_datetime = timezone.now()
+            call.save()
+            return redirect('success')
+        else:
+            call = form.save(commit=False)
+            call.audit_status = "FAILURE"  # Set to FAILURE if the form is invalid
+            call.call_datetime = timezone.now()
+            call.save()  # Optionally save it even if invalid (if it is appropriate for your use case)
+            return render(request, 'failure.html', {'message': 'Call audit failed!', 'form': form})
+
+    else:
+        form = AuditForm()
+
+    return render(request, 'call_auditor.html', {'form': form})
+
+def success_view(request):
+    return render(request, 'success.html', {'message': 'Call audit saved successfully!'})
+
+def failure_view(request):
+    return render(request, 'failure.html', {'message': 'Call audit failed!'})
